c50addd78ef26db2e6fd5e2bdd001c19d701e45f
kazanzhi <z.kazanzhi2017@gmail.com>
2023-01-24 18:11:30 +0200

    Initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Core.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await context.Products.ToListAsync();
+            return Ok(products);
+        }
+            
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id) 
+        {
+            var product = await context.Products.FindAsync(id);
+            if (product == null)
+                return BadRequest("Product not found!");
+            return Ok(product);
+        }
+    }
+}
